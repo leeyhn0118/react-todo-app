827ddb89e0ee4973ca50a75dff2ec350bed11c27
YOUNGHUN LEE <158002370+leeyhn0118@users.noreply.github.com>
2025-06-27 11:13:01 -0400

    Edited

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import "./App.css";
 import Lists from "./components/Lists";
 import Form from "./components/Form";
 
-const initialTodoData = localStorage.getItem("todoData") ? JSON.parse(localStorage.getItem("todoData")) : [];
+const initialTodoData = localStorage.getItem("todoData")
+  ? JSON.parse(localStorage.getItem("todoData"))
+  : [];
 
 function App() {
-  console.log('App Component')
+  console.log("App Component");
   const [todoData, setTodoData] = useState(initialTodoData);
   const [value, setValue] = useState("");
 
-  const handleClick = useCallback((id) => {
-    let newTodoData = todoData.filter(data => data.id !== id)
-    setTodoData(newTodoData)
-    localStorage.setItem("todoData", JSON.stringify(newTodoData))
-  }, [todoData]);
+  const handleClick = useCallback(
+    (id) => {
+      let newTodoData = todoData.filter((data) => data.id !== id);
+      setTodoData(newTodoData);
+      localStorage.setItem("todoData", JSON.stringify(newTodoData));
+    },
+    [todoData]
+  );
 
   const handleRemoveClick = () => {
-    setTodoData([])
-    localStorage.setItem("todoData", JSON.stringify([]))
+    setTodoData([]);
+    localStorage.setItem("todoData", JSON.stringify([]));
   };
 
   const handleSubmit = (e) => {
@@ -31,22 +36,26 @@ function App() {
     };
 
     setTodoData((prev) => [...prev, newTodo]);
-    localStorage.setItem("todoData", JSON.stringify([...todoData, newTodo]))
+    localStorage.setItem("todoData", JSON.stringify([...todoData, newTodo]));
     setValue("");
-  }
+  };
 
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
-      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
-        <div className="flex justify-between mb-3">
-          <h1>할 일 목록</h1>
+    <div className='flex items-center justify-center w-screen h-screen bg-blue-100'>
+      <div className='w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg'>
+        <div className='flex justify-between mb-3'>
+          <h1>To-do List</h1>
           <button onClick={handleRemoveClick}>Delete All</button>
         </div>
-        <Lists handleClick={handleClick} todoData={todoData} setTodoData={setTodoData} />
+        <Lists
+          handleClick={handleClick}
+          todoData={todoData}
+          setTodoData={setTodoData}
+        />
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,83 +1,119 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const List = React.memo(({
-    handleClick, id, title, completed, todoData, setTodoData, provided, snapshot
-}) => {
-    console.log('List component')
+const List = React.memo(
+  ({
+    handleClick,
+    id,
+    title,
+    completed,
+    todoData,
+    setTodoData,
+    provided,
+    snapshot,
+  }) => {
+    console.log("List component");
 
-    const [isEditing, setIsEditing] = useState(false)
-    const [editedTitle, setEditedTitle] = useState(title)
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
 
     const handleCompleteChange = (id) => {
-        let newTodoData = todoData.map(data => {
-            if (data.id === id) {
-                data.completed = !data.completed;
-            }
-            return data;
-        });
-        setTodoData(newTodoData)
-        localStorage.setItem("todoData", JSON.stringify(newTodoData))
-    }
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.completed = !data.completed;
+        }
+        return data;
+      });
+      setTodoData(newTodoData);
+      localStorage.setItem("todoData", JSON.stringify(newTodoData));
+    };
 
     const handleEditChange = (event) => {
-        setEditedTitle(event.target.value);
-    }
+      setEditedTitle(event.target.value);
+    };
 
     const handleSubmit = (event) => {
-        event.preventDefault();
+      event.preventDefault();
 
-        let newTodoData = todoData.map(data => {
-            if (data.id === id) {
-                data.title = editedTitle
-            }
-            return data;
-        })
-        setTodoData(newTodoData)
-        localStorage.setItem("todoData", JSON.stringify(newTodoData))
-        setIsEditing(false);
-    }
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.title = editedTitle;
+        }
+        return data;
+      });
+      setTodoData(newTodoData);
+      localStorage.setItem("todoData", JSON.stringify(newTodoData));
+      setIsEditing(false);
+    };
     if (isEditing) {
-        return (
-            <div className={`flex items-center justify-between w-full px-4 py-1 text-gray-600 bg-gray-100 border rounded`}>
-                <div className='items-center'>
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            value={editedTitle}
-                            onChange={handleEditChange}
-                            className='w-full px-3 py-2 mr-4 text-gray-500 rounded'
-                        />
-                    </form>
-                </div>
-                <div className='items-center'>
-                    <button className='px-4 py-2 float-right' onClick={() => setIsEditing(false)}>x</button>
-                    <button onClick={handleSubmit} className='px-4 py-2 float-right' type='submit'>save</button>
-                </div>
-            </div>
-        )
+      return (
+        <div
+          className={`flex items-center justify-between w-full px-4 py-1 text-gray-600 bg-gray-100 border rounded`}
+        >
+          <div className='items-center'>
+            <form onSubmit={handleSubmit}>
+              <input
+                value={editedTitle}
+                onChange={handleEditChange}
+                className='w-full px-3 py-2 mr-4 text-gray-500 rounded'
+              />
+            </form>
+          </div>
+          <div className='items-center'>
+            <button
+              className='px-4 py-2 float-right'
+              onClick={() => setIsEditing(false)}
+            >
+              x
+            </button>
+            <button
+              onClick={handleSubmit}
+              className='px-4 py-2 float-right'
+              type='submit'
+            >
+              save
+            </button>
+          </div>
+        </div>
+      );
     } else {
-        return (
-            <div
-                key={id}
-                {...provided.draggableProps}
-                ref={provided.innerRef}
-                {...provided.dragHandleProps}
-                className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                    } flex items-center justify-between w-full px-4 py-1 text-gray-600 bg-gray-100 border rounded`}>
-                <div className='items-center'>
-                    <input
-                        type="checkbox"
-                        onChange={() => handleCompleteChange(id)}
-                        defaultChecked={completed}
-                    />{" "}
-                    <span className={completed ? "line-through" : undefined}>{title}</span>
-                </div>
-                <div className='items-center'>
-                    <button className='px-4 py-2 float-right' onClick={() => handleClick(id)}>x</button>
-                    <button className='px-4 py-2 float-right' onClick={() => setIsEditing(true)}>edit</button>
-                </div>
-            </div>
-        )
+      return (
+        <div
+          key={id}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+          className={`${
+            snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+          } flex items-center justify-between w-full px-4 py-1 text-gray-600 bg-gray-100 border rounded`}
+        >
+          <div className='items-center'>
+            <input
+              type='checkbox'
+              onChange={() => handleCompleteChange(id)}
+              defaultChecked={completed}
+            />{" "}
+            <span className={completed ? "line-through" : undefined}>
+              {title}
+            </span>
+          </div>
+          <div className='items-center'>
+            <button
+              className='px-4 py-2 float-right'
+              onClick={() => handleClick(id)}
+            >
+              X
+            </button>
+            <button
+              className='px-4 py-2 float-right'
+              onClick={() => setIsEditing(true)}
+            >
+              Edit
+            </button>
+          </div>
+        </div>
+      );
     }
-});
+  }
+);
 
-export default List
+export default List;
